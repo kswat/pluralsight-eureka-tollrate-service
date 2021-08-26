c68f372aa51507b8a8b0f772432bb3579bd2447e
VKK <kishore.saraswathula@googlemail.com>
2021-08-26 18:15:18 +0100

    Initial commit

---
+++ src/test/java/pluralsight/demo/PluralsightEurekaTollrateServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package pluralsight.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class PluralsightEurekaTollrateServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
